8a2ae6897f9bf1dde6c6b39c457f4bf3b1d4a452
zhaochenchen <ialwayswannafly@yahoo.com>
2017-10-31 21:52:31 +0800

    develop

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import Oasis from './oasis.jpg';
+import pringMe from './print';
 function component() {
   var element = document.createElement('div');
-
+  var btn = document.createElement('button');
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var oasis = new Image();
-  oasis.src = Oasis;
-  element.appendChild(oasis);
+  btn.innerHTML = 'Click me and check the console!';
+
+  btn.onclick = pringMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
-      {
-        test: /\.((png|svg|jpg|gif)$)/,
-        use: [
-          'file-loader'
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ],
-      }
-    ]
-  }
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    })
+  ]
 }
