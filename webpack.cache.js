8c0704cc50287894d11feee30a40bcc2d42da3b4
zhaochenchen <ialwayswannafly@yahoo.com>
2017-11-06 21:45:19 +0800

    cache

--- webpack.cache.js
+++ webpack.cache.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js',entry: {
+    main: './src/index.js',
+    vendor: [
+     'lodash'
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Caching'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
     })
   ],
   output: {
